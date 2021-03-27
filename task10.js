8b404c4880663d4b7faa9b7138a0c35274682777
Heavens Kehinde <ken_adex@yahoo.com>
2021-03-27 14:26:11 +0100

    Challenge with task10

--- task10.js
+++ task10.js
@@ -1,3 +1,20 @@
+var firstString = "running";
+var secondString = "dancing";
+
+var newString;
+var notCommon;
+
+for(i = 0; i < firstString.length; i++){
+    //console.log(firstString[i]);
+    for(i = 0; i < secondString.length; i++){
+        if(secondString[i] === firstString[1]){
+            newString += firstString[i];
+        }
+    }
+}
+
+console.log(goaway);
+
 
 
 
